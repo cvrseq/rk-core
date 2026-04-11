d1b28269ca2c76cc559eb3cc09592012cf049c1e
a <agasha@Aleksandrs-MacBook-Air.local>
2026-04-11 10:51:58 -0700

    feat: update

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,7 +1,7 @@
 package service
 
 type StatementService interface {
-	CreateConfig(cfg Model) (Model, error)
+	CreateConfig(cfg Model) (*Model, error)
 	GetUserById(id int) (*Model, error)
 	GetUsersList() ([]Model, error)
 	GetOrderById(orderId int) (*Model, error)
@@ -16,9 +16,23 @@ func NewVpnService(r UserDataRepository) StatementService {
 	return &VpnService{repo: r}
 }
 
-func (s *VpnService) CreateConfig(cfg Model) error {
-	if err := s.repo.CreateConfig(&cfg); err != nil {
-		return Model{}, err
-	}
-	return cfg, nil
+func (s *VpnService) CreateConfig(cfg Model) (*Model, error) {
+
+	return nil, nil
+}
+
+func (s *VpnService) GetUserById(id int) (*Model, error) {
+	return nil, nil
+}
+
+func (s *VpnService) GetUsersList() ([]Model, error) {
+	return nil, nil
+}
+
+func (s *VpnService) GetOrderById(orderId int) (*Model, error) {
+	return nil, nil
+}
+
+func (s *VpnService) GetOrdersList() ([]Model, error) {
+	return nil, nil
 }
