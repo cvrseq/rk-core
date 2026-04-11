9891d0d81748b0ee7752215e4096f69a87c58133
a <agasha@Aleksandrs-MacBook-Air.local>
2026-04-11 11:43:27 -0700

    feat: refactoring

--- internal/service/orm.go
+++
@@ -1,9 +0,0 @@
-package service
-
-type Model struct {
-	ID       uint   `gorm:"primaryKey" json:"id"`
-	OrderID  uint   `gorm:"primaryKey" json:"order_id"`
-	NickName string `gorm:"column:nick_name" json:"nick_name"`
-	Region   string `gorm:"column:region" json:"region"`
-	IsDone   bool   `gorm:"column:is_done" json:"is_done"`
-}
