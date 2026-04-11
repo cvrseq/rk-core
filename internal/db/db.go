d9e0a6cee7328ee55c350ba6963f9418eec6b282
a <agasha@Aleksandrs-MacBook-Air.local>
2026-04-11 07:16:30 -0700

    feat: db and repository pattern realisation

--- internal/db/db.go
+++ internal/db/db.go
@@ -1,1 +1,23 @@
 package db
+
+import (
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+func InitDB() (*gorm.DB, error) {
+	dataSrcName := "host=db user=postgres password=devpassword dbname=database port=5432 sslmode=disable"
+
+	var err error
+
+	db, err = gorm.Open(postgres.Open(dataSrcName), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Could not connect to database: %v", err)
+	}
+
+	return db, nil
+}
--- internal/service/repository.go
+++ internal/service/repository.go
@@ -1,1 +1,8 @@
 package service
+
+type UserDataRepository interface {
+	GetUserById(id int) (*Model, error)
+	GetUsersList() ([]Model, error)
+	GetOrderById(orderId int) (*Model, error)
+	GetOrdersList() ([]Model, error) 
+}
